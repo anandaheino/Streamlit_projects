cb00f3cd6288a8ad9d65a3b5dc9ff32d89204ae8
AnandaHHC <anandaheino2@gmail.com>
2021-09-14 17:34:50 -0300

    Displaying the DataFrame retrieved from yfinance.

--- Stock_Price/app.py
+++ Stock_Price/app.py
@@ -8,12 +8,15 @@ Shown are the stock closing proce and volume of ![img](https://99prod.s3.amazona
 
 """)
 
+
+#adding a selectbox for the start year
 year = 2010
 years = ['2010', '2011', '2012', '2013', 2014, '2015', '2016', '2017', '2018', '2019']
-year = st.selectbox("Select the start year:", years)
+st.write("## Select the start year:")
+year = st.selectbox('Options:', years)
 date = f'{year}-5-31'
 
-st.write("You selected the year ", year)
+st.write(f"Ok! lets go from {year} to 2020.")
 
 # defining the ticker symbol to pass to yf
 tickerSymbol = 'GOOGL'
@@ -22,6 +25,13 @@ tickerData = yf.Ticker(tickerSymbol)
 # getting the historical prices 
 tickerDf = tickerData.history(period='1d', start=date, end='2020-5-31')
 
+# displaying the dataframe
+st.write("""
+## Want to take a look at the data retrieved?
+Let me show you a piece of the table:
+""")
+
+st.write(tickerDf)
 
 st.write("## Closing Price")
 st.line_chart(tickerDf.Close)
