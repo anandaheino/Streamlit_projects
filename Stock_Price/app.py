51fef86c842e62327e21e40d6015448e7ad61980
AnandaHHC <anandaheino2@gmail.com>
2021-09-14 17:26:02 -0300

    Adding a selectbox for the start year, and headers.

--- Stock_Price/app.py
+++ Stock_Price/app.py
@@ -8,12 +8,23 @@ Shown are the stock closing proce and volume of ![img](https://99prod.s3.amazona
 
 """)
 
+year = 2010
+years = ['2010', '2011', '2012', '2013', 2014, '2015', '2016', '2017', '2018', '2019']
+year = st.selectbox("Select the start year:", years)
+date = f'{year}-5-31'
+
+st.write("You selected the year ", year)
+
 # defining the ticker symbol to pass to yf
 tickerSymbol = 'GOOGL'
 # getting data from this ticker
 tickerData = yf.Ticker(tickerSymbol)
 # getting the historical prices 
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+tickerDf = tickerData.history(period='1d', start=date, end='2020-5-31')
 
+
+st.write("## Closing Price")
 st.line_chart(tickerDf.Close)
+
+st.write("## Volume Price")
 st.line_chart(tickerDf.Volume)
